b5ea7dd1e8c17cbe6b07fe74fed9b522b99357e0
KazeBox33 <xdj483829269@163.com>
2026-06-15 15:57:03 +0800

    Implement generation with KV cache

--- src/tiny_llm/generate.py
+++ src/tiny_llm/generate.py
@@ -3,7 +3,7 @@ from mlx_lm.tokenizer_utils import TokenizerWrapper
 from .qwen3_week1 import Qwen3ModelWeek1
 from .qwen3_week2 import Qwen3ModelWeek2
 from typing import Callable
-
+from .kv_cache import TinyKvFullCache
 
 def simple_generate(
     model: Qwen3ModelWeek1,
@@ -44,8 +44,35 @@ def simple_generate(
 def simple_generate_with_kv_cache(
     model: Qwen3ModelWeek2, tokenizer: TokenizerWrapper, prompt: str
 ) -> str:
+    kv_cache=[TinyKvFullCache() for _ in range(model.num_hidden_layers)] #不同的层数对应不同的kv_cache
+
     def _step(model, y, offset, kv_cache):
-        pass
+        logits=model(y[None],offset,kv_cache)
+        logits=logits[:,-1,:]
+
+        logprobs=logits-mx.logsumexp(logits,keepdims=True)
+        y=mx.argmax(logprobs,axis=-1)
+        return y
+
+    tokens=mx.array(tokenizer.encode(prompt,add_special_tokens=False))
+
+    detokenizer=tokenizer.detokenizer
+    detokenizer.reset()
+
+    offset=0
+
+    while True:  # 这里最大的改动就是 1. 加入并随着步骤更新tokens ， 2 再把下一轮输入改成刚生成的单个token
+        token=_step(model,tokens,offset,kv_cache)
+        mx.eval(token)
+
+        if token.item()==tokenizer.eos_token_id:
+            break
+
+        detokenizer.add_token(token.item())
+        print(detokenizer.last_segment,end="",flush=True)
+
+        offset+=tokens.size
+        tokens=token
 
 def speculative_generate(
     draft_model: Qwen3ModelWeek2,
